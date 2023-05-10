235c12b337d3f9acb4ce9340ea9764f546bfe0ea
Isabela Amaro <isabela.souza5@fatec.sp.gov.br>
2023-05-10 23:59:06 +0000

    finalizando a classe livro

--- src/main/java/models/Livro.java
+++ src/main/java/models/Livro.java
@@ -1,11 +1,59 @@
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MainyToOne;
+
 
 
 @Entity 
 @Table(name = 'livros')
 
 public class Livro{
+
+
+    private int id;
+    private String titulo;
+
+  public void setTitulo(String titulo){
+    this.titulo = titulo;
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+  public void setId(int id){
+    this.id = id;
+
+    public int getId(int id){
+        return id;
+    }
+
+    
+@MainyToOne
+@JoinColumn(name = 'id_autor')
+private Autor autor;
+
+public void setAutor(Autor autor){
+    this.autor = autor;
+}
+public Autor getAutor(){
+    return autor;
+}
+
+@MainyToOne
+@JoinColumn(name = 'id_genero')
+private Genero genero;
+
+public void setGenero(Genero genero){
+    this.genero = genero;
+}
+public Genero getGenero(){
+    return genero;
+}
+
+}
+
+  }
 
 }
